fb245fb52aea09f0dba2be7242bf18abb3f6a2fd
YomoABH <92528398+YomoABH@users.noreply.github.com>
2023-01-14 19:51:43 +0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,34 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    document.querySelectorAll(".input-file").forEach(((el, i) => {
+        el.addEventListener("change", (event => {
+            console.log();
+            document.querySelectorAll(".input-file-text")[i];
+            console.log(document.querySelectorAll(".input-file-text")[i].textContent = event.target.value);
+        }));
+    }));
+    window["FLS"] = true;
+    isWebp();
+})();
